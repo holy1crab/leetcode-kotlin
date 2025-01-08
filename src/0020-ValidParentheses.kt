ba6cc5317bbea6230ad1fba06c353d14822ba30b
holy1crab <holy1crab@gmail.com>
2025-01-08 12:46:31 +0500

    add solutions

---
+++ src/0020-ValidParentheses.kt
@@ -0,0 +1,69 @@
+import kotlin.collections.ArrayDeque
+
+fun isValidParentheses(s: String): Boolean {
+
+    val stack = ArrayDeque<Char>()
+    val viceVersa = mapOf('(' to ')', '[' to ']', '{' to '}')
+
+    for (char in s) {
+        if (char == '(' || char == '[' || char == '{') {
+            stack.addLast(char)
+        } else {
+            if (stack.isEmpty() || viceVersa[stack.removeLast()] != char) {
+                return false
+            }
+        }
+    }
+
+    return stack.isEmpty()
+}
+
+
+fun main() {
+    println(!isValidParentheses("((({"))
+    println(isValidParentheses("()"))
+    println(isValidParentheses("([])"))
+    println(!isValidParentheses("([[])"))
+}
+
+/*
+Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+
+An input string is valid if:
+
+Open brackets must be closed by the same type of brackets.
+Open brackets must be closed in the correct order.
+Every close bracket has a corresponding open bracket of the same type.
+
+
+Example 1:
+
+Input: s = "()"
+
+Output: true
+
+Example 2:
+
+Input: s = "()[]{}"
+
+Output: true
+
+Example 3:
+
+Input: s = "(]"
+
+Output: false
+
+Example 4:
+
+Input: s = "([])"
+
+Output: true
+
+
+
+Constraints:
+
+1 <= s.length <= 104
+s consists of parentheses only '()[]{}'.
+ */
